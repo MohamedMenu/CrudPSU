d921e22d9ca1c49a182a944019af0333dba9b167
MOHAMED ABDULKADIR <MOHAMED ABDULKADIR@DESKTOP-1TO2SV1>
2025-06-18 03:24:22 +0300

    kan 3aad

--- CrudPSU/Program.cs
+++ CrudPSU/Program.cs
@@ -1,4 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Welcome to PSU univeristy");
 Console.WriteLine("Wax ayaa kabadalnay"); 
-const
+Console.Write("Tijaabo 2aad")
